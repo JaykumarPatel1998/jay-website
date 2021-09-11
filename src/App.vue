6d5ea88ce2e211d29900b563b34a51e618b773e8
jaykumarpatel2710@gmail.com <jaykumarpatel2710@gmail.com>
2021-09-11 16:09:57 +0530

    new commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
-  <h1>Jaykumar Patel</h1>
-  <LandingPage/>
+  <img class="display-picture" src="./assets/jay.jpg" alt="" srcset="">
+  <h1 class="namespace">&lt; Jaykumar Patel /&gt;</h1>
+  <LandingPage />
 </template>
 
 <script>
-import LandingPage from './components/LandingPage.vue'
+import LandingPage from "./components/LandingPage.vue";
+import gsap from "gsap";
+const tl = gsap.timeline({ ease: "SteppedEase.config(55)" });
 
 export default {
   name: "App",
   components: {
     LandingPage,
   },
-  data(){
-    return{
-    }
-  }
+  data() {
+    return {};
+  },
+  mounted: function () {
+   
+    tl.fromTo(
+      ".namespace",
+      {
+        width: "0",
+      },
+      {
+        duration: 4,
+        width: "75%",
+      }
+    );
+  },
 };
 </script>
 
@@ -28,13 +43,21 @@ export default {
   width: 100%;
 }
 
-h1{
+h1 {
+  white-space: nowrap;
+  overflow: hidden;
+  box-sizing: content-box;
   margin: 20px auto;
-  width: 65%;
+  width: 0;
   color: rgb(206, 29, 197);
-  background: black;
   padding: 10px;
   border-radius: 5px;
 }
 
+.display-picture{
+  max-width: 20%;
+  border: 1px solid;
+  border-radius: 20%;
+  margin-top: 20px;
+}
 </style>
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="hello">
     <div class="items clickable">
-      <div>
         &lt;<input class="placeholder"
           v-model="placeholder"
           placeholder="education,social &#38; skills"
           type="text"
           maxlength="18"
         />
-      </div>
     </div>
     <div class="items interactive">
-      <div class="enter">&lt; {{ placeholder }} /&gt;</div>
+      <div v-on:click="triggerAnimation()" class="enter">&lt; {{ placeholder }} /&gt;</div>
     </div>
   </div>
 </template>
 
 <script>
 import { gsap } from "gsap";
+const tl = gsap.timeline({ paused: true });
 
 export default {
   name: "LandingPage",
@@ -27,18 +26,28 @@ export default {
     };
   },
   mounted: function () {
-    const tl = gsap.timeline({ paused: true });
-    document.querySelector(".enter").addEventListener("click", () => {
-      tl.resume();
-    });
     tl.to(".interactive", {
       duration: 0.5,
       color: "rgb(243, 4, 252)",
       ease: "power1.out",
       width: "60%",
       height: "100px",
-    })
+    });
+    setTimeout(() => {
+        document.querySelector('.placeholder').focus()
+    }, 3000);
   },
+  methods:{
+      triggerAnimation(){
+          
+    document.querySelector(".interactive").addEventListener("click", () => {
+      tl.play();
+    });
+    document.querySelector(".placeholder").addEventListener("click", () => {
+      tl.reverse();
+    });
+      }
+  }
 };
 </script>
 
@@ -47,7 +56,7 @@ export default {
 .hello {
   display: grid;
   place-items: center;
-  grid-template-rows: 2fr 3fr;
+  grid-template-columns: 1fr;
   border: 2px solid black;
   background: rgba(22, 21, 21, 1);
   width: 70%;
@@ -63,7 +72,6 @@ h1 {
   color: rgb(206, 29, 197);
 }
 .enter {
-  border: 1px solid whitesmoke;
   border-radius: 2px;
   padding: 5px;
 }
@@ -72,6 +80,10 @@ h1 {
   color: rgb(206, 29, 197);
   border: 2px solid rgb(206, 29, 197);
   padding: 5%;
+  width: 50%;
+}
+.clickable{
+    margin: 20px;
 }
 .placeholder{
     background: black;
